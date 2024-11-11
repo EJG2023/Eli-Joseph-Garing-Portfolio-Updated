bd0db1338375b177a73e1dbba733ec933a735d8d
EJG2023 <133731036+EJG2023@users.noreply.github.com>
2024-11-11 16:05:38 -0600

    Theme Completed, Project next

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,21 +1,35 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/grad-img.png';
-import themeIcon from '../../assets/sun.svg';
-import twitterIcon from '../../assets/twitter-light.svg';
-import githubIcon from '../../assets/github-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
-import CV from '../../assets/Resume.pdf'
+import heroImg from '../../assets/grad-img-modified.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/Resume.pdf';
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
+    const { theme, toggleTheme } = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
   return <section id="hero" className={styles.container}>
     <div className={styles.colorModeContainer}>
         <img className={styles.hero} 
+        /* image needs to be size of 300x300 */ 
         src={heroImg} 
         alt="Profile Picture of Eli Joseph Garing" 
         />
         <img className={styles.colorMode}
         src={themeIcon} 
         alt="Color mode Icon"
+        onClick={toggleTheme}
         />
     </div>
     <div className={styles.info}> 
@@ -32,7 +46,7 @@ function Hero() {
             <img src={linkedinIcon} alt="linkedin Icon"/>
             </a>
         </span>
-        <p>
+        <p className={styles.description}>
             Passion for Web Development using React and Data Analytics
         </p>
         <a href={CV} download>
