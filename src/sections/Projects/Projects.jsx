b530a9f73625c4b91f1d0081e90e7f522b9057d4
EJG2023 <133731036+EJG2023@users.noreply.github.com>
2025-01-09 01:24:28 -0600

    added media queries for dynamic view, might change the spacings between sections

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -12,6 +12,8 @@ function Projects() {
             <ProjectCard src={ArcadeIcon} 
             link="https://github.com/EJG2023/VR-Arcade-Basketball-Application"
             h3="VR Arcade Basketball Game"
+
+            // Keep paragraph into 3 lines max for display
             p="Developed an immersive VR Arcade Basketball game using Unity and Blender, featuring 
                realistic basketball mechanics for a dynamic, arcade-style experience."
             />
@@ -19,7 +21,19 @@ function Projects() {
             link="https://github.com/EJG2023/Online-Grocery-Application-Project"
             h3="Online Grocery Shopping Application"
             p="Developed an Online Grocery Shopping Application with a health-centric experience and 
-            personalized meal-planning, using Python (Flask) and MySQL for backend services, and React for a responsive frontend."
+            personalized meal-planning, using Python (Flask) and MySQL for backend services, and React for frontend."
+            />
+            <ProjectCard src={ArcadeIcon} 
+            link="https://github.com/EJG2023/VR-Arcade-Basketball-Application"
+            h3="VR Arcade Basketball Game"
+            p="Developed an immersive VR Arcade Basketball game using Unity and Blender, featuring 
+               realistic basketball mechanics for a dynamic, arcade-style experience."
+            />
+             <ProjectCard src={BasketIcon} 
+            link="https://github.com/EJG2023/Online-Grocery-Application-Project"
+            h3="Online Grocery Shopping Application"
+            p="Developed an Online Grocery Shopping Application with a health-centric experience and 
+            personalized meal-planning, using Python (Flask) and MySQL for backend services, and React for frontend."
             />       
         </div>
 
